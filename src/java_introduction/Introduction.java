64307a2f81fa951c87901e8ed14feb220ce1fe16
MikiKru <michal_kruczkowski@o2.pl>
2019-11-14 19:34:13 +0100

    java_basic v2

--- src/java_introduction/Introduction.java
+++ src/java_introduction/Introduction.java
@@ -21,6 +21,29 @@ public class Introduction {
         int myFirstNumber = 50;
         System.out.println("myFirstNumber = " + myFirstNumber);
         System.out.println("Dodawanie liczb: " + (100 + myFirstNumber));
+        double salaryNet = 9150.99;
+        int vatTax = 23;
+        System.out.println("Kwota netto: " + salaryNet + "zł");
+        System.out.println("Kwota brutto: " + (salaryNet * (1 + (vatTax/100.0))) + "zł");
+        // formatowanie wyjścia
+        // \n -> newline
+        System.out.printf("Kwota brutto: %.2f zł\n", salaryNet * (1 + (vatTax/100.0)));
+        System.out.printf("%.2fzł netto to %.2fzł brutto\n", salaryNet, salaryNet * (1 + (vatTax/100.0)));
+        System.out.printf("Liczba: %20.2f\n", salaryNet);
+
+        char smallLetter = 'a';
+        char bigLetter = 'A';
+        char dotChar = '.';
+
+        System.out.println(smallLetter);
+        System.out.println((char)(bigLetter + 10));
+        System.out.printf("Znak: %c ma %d numer ASCII\n", dotChar, (int)dotChar);
+
+        boolean isActivated = true;
+        System.out.println(isActivated);
+        System.out.println(!isActivated);
+
+
 
     }
 }
