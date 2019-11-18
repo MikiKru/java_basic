e29c39f3711df6f9ae51a952bc031fb21ae328ff
MikiKru <michal_kruczkowski@o2.pl>
2019-11-18 19:52:01 +0100

    java_vasic d3 v7

--- src/loops/OmittingInstructions.java
+++ src/loops/OmittingInstructions.java
@@ -6,19 +6,31 @@ public class OmittingInstructions {
         // -> JESLI ZNALEZIEONO -> WYPISZ POZYCJE TEGO ZNAKU
         // -> JESLI NIE ZNALEZIONO WYPISZ STOSOWNY KOMUNIKAT
         String sentence = "Ala ma kota a kot ma Alę";
-        char search = 'l';
+        char search = '.';
         boolean isFound = false;
-        for (int i = 0; i < sentence.length() ; i++) {
+        for (int i = 0; i < sentence.length(); i++) {
             System.out.println("Iteracja nr. " + i);
-            if(sentence.charAt(i) == search){
-                System.out.printf("Znaleziono znak %c na indeksie %d\n", sentence.charAt(i),i);
+            if (sentence.charAt(i) != search) {
+                // pominięcie aktualnie wykonywanej iteracji i przejście do kolejnej
+                continue;
+            } else if (sentence.charAt(i) == search) {
+                System.out.printf("Znaleziono znak %c na indeksie %d\n", sentence.charAt(i), i);
                 isFound = true;
                 // instrukcja przerywająca działanie pętli
                 break;
             }
+            System.out.println("Nic ważnego");
         }
-        if(!isFound){
+        if (!isFound) {
             System.out.printf("Nie znaleziono znaku %c w tekście %s\n", search, sentence);
         }
+        System.out.println("======================================");
+        for (int i = 0; i < 10; i++) {
+            if (i == 5)
+                continue;
+            System.out.println(i);
+        }
+
+
     }
 }
