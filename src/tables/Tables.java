cb6425ebca7bcc816f97b4b7164d9eb8f860b72e
MikiKru <michal_kruczkowski@o2.pl>
2019-11-18 20:07:29 +0100

    java_vasic d3 v8

---
+++ src/tables/Tables.java
@@ -0,0 +1,21 @@
+package tables;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Tables {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // deklaracja
+        String[] names = new String[5];
+        // przypisanie wartości
+        for (int i = 0; i < names.length; i++) {
+            System.out.println("Wprowadź imię: ");
+            names[i] = scanner.nextLine();
+        }
+//        for (String name : names) {
+//            System.out.println(name);
+//        }
+        Arrays.stream(names).forEach(System.out::println);
+    }
+}
