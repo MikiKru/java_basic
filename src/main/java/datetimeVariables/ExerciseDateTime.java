faf8896327ff6eb0d4ac15c3dff463019bc97062
MikiKru <michal_kruczkowski@o2.pl>
2019-11-21 20:24:08 +0100

    java_basic datetime v2

--- src/main/java/datetimeVariables/ExerciseDateTime.java
+++ src/main/java/datetimeVariables/ExerciseDateTime.java
@@ -1,8 +1,22 @@
 package datetimeVariables;
 
+import java.time.LocalDate;
+
 public class ExerciseDateTime {
     public static void main(String[] args) {
         // znajdź dzień tygodnia w którym się urodziałeś/urodziałaś
-        
+        LocalDate birthDate = LocalDate.of(1977,4,1);
+        int myDayOfWeek = birthDate.getDayOfWeek().getValue();
+        System.out.println(myDayOfWeek);
+        // znajdź ile razy do dziś Twoje urodziny przypadały
+        // w ten sam dzień tygodnia w którym się urodziłeś/urodziłaś
+        LocalDate actual = LocalDate.now();
+        int counter = 0;
+        for (birthDate.getYear(); birthDate.getYear() <= actual.getYear(); birthDate = birthDate.plusYears(1)) {
+            if(myDayOfWeek == birthDate.getDayOfWeek().getValue()){
+                System.out.println(birthDate);
+                counter++;
+            }
+        }
     }
 }
