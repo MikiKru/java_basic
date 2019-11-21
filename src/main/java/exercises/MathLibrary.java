204ed34e4d2e756e795d435cacca7cb7b2318d40
MikiKru <michal_kruczkowski@o2.pl>
2019-11-21 17:46:33 +0100

    java_basic exercises

---
+++ src/main/java/exercises/MathLibrary.java
@@ -0,0 +1,17 @@
+package exercises;
+
+public class MathLibrary {
+    // 1. Zaimplementuj metodę zwracającą średnią wartości
+    // typu double podanych w argumencie metody jak tablica
+    public static double avg(double [] args){
+        return 0.0;
+    }
+    // 2. Zaimplementuj metodę potęgującą dwie liczby całkowite
+    // Metoda zwraca wynik działania a przyjmuje jako argumenty kolejno
+    // podstawę i wykładnik potęgi
+    public static int power(int x, int y){
+        return 0;
+    }
+
+
+}
