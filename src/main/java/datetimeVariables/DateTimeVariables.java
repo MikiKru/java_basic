2ef82ff5d1455f21ddc3dda2ed8c431ab05cf517
MikiKru <michal_kruczkowski@o2.pl>
2019-11-21 20:11:30 +0100

    java_basic datetime v1

--- src/main/java/datetimeVariables/DateTimeVariables.java
+++ src/main/java/datetimeVariables/DateTimeVariables.java
@@ -1,7 +1,7 @@
 package datetimeVariables;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.time.*;
+import java.time.temporal.Temporal;
 
 public class DateTimeVariables {
     public static void getTimeMethods(){
@@ -20,17 +20,36 @@ public class DateTimeVariables {
         } else {
             System.out.println("czas: " + custom + " jest później");
         }
+        Duration duration = Duration.between(custom, actual);
+        System.out.println(duration);
     }
     public static void getDateMethods(){
         LocalDate actulDate = LocalDate.now();
-        LocalDate customDate = LocalDate.of(2000,5,21);
+        LocalDate customDate = LocalDate.of(2000,5,22);
         System.out.println(actulDate);
         System.out.println(customDate);
 
+        // porównanie dat
+        Period period = Period.between(customDate, actulDate); // różnica arg2 - arg1
+        System.out.println(period);
+        System.out.println(period.getYears());
+        System.out.println(period.getMonths());
+        System.out.println(period.getDays());
+
+    }
+
+    public static void getDateTimeMethods(){
+        LocalDateTime actual = LocalDateTime.now();
+        LocalDateTime custom = LocalDateTime.of(2000,3,2,10,10,10);
+        System.out.println(actual);
+        System.out.println(custom);
+
+        System.out.println(actual.compareTo(custom));
     }
 
     public static void main(String[] args) {
-//        getTimeMethods();
-    getDateMethods();
+//          getTimeMethods();
+//          getDateMethods();
+        getDateTimeMethods();
     }
 }
---
+++ src/main/java/datetimeVariables/ExerciseDateTime.java
@@ -0,0 +1,8 @@
+package datetimeVariables;
+
+public class ExerciseDateTime {
+    public static void main(String[] args) {
+        // znajdź dzień tygodnia w którym się urodziałeś/urodziałaś
+        
+    }
+}
