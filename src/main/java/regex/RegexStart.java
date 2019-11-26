986e013432d72e37bf967850fbf7bd99b93d8cca
MikiKru <michal_kruczkowski@o2.pl>
2019-11-26 18:58:09 +0100

    regexp password valid v1

--- src/main/java/regex/RegexStart.java
+++ src/main/java/regex/RegexStart.java
@@ -6,6 +6,8 @@ import java.util.regex.Pattern;
 public class RegexStart {
     public static void main(String[] args) {
         while(true) {
+            System.out.println("Podaj adres email użytkownika");
+            String email = new Scanner(System.in).nextLine();
             System.out.println("Podaj hasło użytkownika");
             String password = new Scanner(System.in).nextLine();
             // pattern to nasz wzorzec zawierający wyrażenia regularne
@@ -18,12 +20,18 @@ public class RegexStart {
             // jedna cyfra
             // jeden znak specjalny {_-.&}
             // bez zpacji
+            String patternEmail = "^([a-z]|[A-Z]|[0-9]|[._#]|[^\\s\\t]){1,}@([a-z]|[A-Z]|[0-9]|[._#]|[^\\s\\t]){1,}\\.([a-z]|[A-Z]|[^\\s\\t]){2,}$";
+            if(Pattern.matches(patternEmail,email)){
+                System.out.println("Adres e-mail jest ok");
+            } else {
+                System.out.println("Błędyn adres email");
+            }
             String patternLowerCase = ".*[a-z]{1,}.*";
             String patternUpperCase = ".*[A-Z]{1,}.*";
             String patternDigit = ".*[0-9]{1,}.*";
             String patternSpecialCharacter = ".*[-_.&]{1,}.*";
             String patternWithoutSpace = ".*[\\s|\\t]{1,}.*";
-
+            // walidacja hasła
             if(password.length() < 8){
                 System.out.println("Hasło musi zawierać co najmniej 8 znaków");
             } else if (!Pattern.matches(patternLowerCase, password)){
