5a28ee467d5262e4b7dea4277ea30eaaa5c4f0f6
MikiKru <michal_kruczkowski@o2.pl>
2019-11-26 20:06:59 +0100

    auth v5

--- src/main/java/authenticationTemplate/controller/UserController.java
+++ src/main/java/authenticationTemplate/controller/UserController.java
@@ -3,6 +3,8 @@ package authenticationTemplate.controller;
 import authenticationTemplate.model.User;
 import authenticationTemplate.utility.InMemoryDB;
 
+import java.util.regex.Pattern;
+
 public class UserController {
     // serwis wypisujący na ekranie wszystkich użytkowników z tabelki user
     public void getAllUsers(){
@@ -21,6 +23,49 @@ public class UserController {
             }
         }
         return user;
+    }
+    private boolean emailValid(String email){
+        String patternEmail = "^([a-z]|[A-Z]|[0-9]|[._#]|[^\\s\\t@]){1,}@([a-z]|[A-Z]|[0-9]|[._#]|[^\\s\\t@]){1,}\\.([a-z]|[A-Z]|[^\\s\\t@]){2,}$";
+        boolean emailValidResult = false;
+        if(Pattern.matches(patternEmail,email)){
+            emailValidResult = true;
+        }
+        return emailValidResult;
+    }
+    private String passwordValid(String password){
+        String patternLowerCase = ".*[a-z]{1,}.*";
+        String patternUpperCase = ".*[A-Z]{1,}.*";
+        String patternDigit = ".*[0-9]{1,}.*";
+        String patternSpecialCharacter = ".*[-_.&]{1,}.*";
+        String patternWithoutSpace = ".*[\\s|\\t]{1,}.*";
+
+        String passwordValidResult = null;
+        if(password.length() < 8){
+            passwordValidResult = "Hasło musi zawierać co najmniej 8 znaków";
+        } else if (!Pattern.matches(patternLowerCase, password)){
+            passwordValidResult = "Hasło musi posiadać co najmniej jedną małą literę";
+        } else if (!Pattern.matches(patternUpperCase, password)) {
+            passwordValidResult = "Hasło musi posiadać co najmniej jedną wielką literę";
+        } else if (!Pattern.matches(patternDigit, password)){
+            passwordValidResult = "Hasło musi posiadać co najmniej jedną cyfre";
+        } else if (!Pattern.matches(patternSpecialCharacter, password)){
+            passwordValidResult = "Hasło musi posiadać co najmniej jeden znak specjalny {-_.&}";
+        } else if (Pattern.matches(patternWithoutSpace, password)) {
+            passwordValidResult = "Hasło nie może zawierać znaków białych";
+        }
+        return passwordValidResult;
+    }
+    public String registerUser(String email, String password){
+        String registerResult = "";
+        if(emailValid(email)){
+
+        } else {
+            registerResult = "Błędny adres e-mail";
+        }
+
+        User user = new User(email,password);
+        InMemoryDB.tableUser.add(user);
+        return user.toString();
     }
 
 
