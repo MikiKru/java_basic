01394731f7308871e949421bba8b3ab7f768f300
MikiKru <michal_kruczkowski@o2.pl>
2019-11-26 19:43:06 +0100

    auth v3

--- src/main/java/authenticationTemplate/Main.java
+++ src/main/java/authenticationTemplate/Main.java
@@ -1,7 +1,10 @@
 package authenticationTemplate;
 
+import authenticationTemplate.controller.UserController;
+
 public class Main {
     public static void main(String[] args) {
-
+        UserController uc = new UserController();
+        uc.getAllUsers();
     }
 }
--- src/main/java/authenticationTemplate/controller/UserController.java
+++ src/main/java/authenticationTemplate/controller/UserController.java
@@ -6,9 +6,9 @@ import authenticationTemplate.utility.InMemoryDB;
 public class UserController {
     // serwis wypisujący na ekranie wszystkich użytkowników z tabelki user
     public void getAllUsers(){
-        System.out.printf("| %3s | %20s | %20s | %20s |\n", "ID", "EMAIL", "PASSWORD", "REGISTRATION");
+        System.out.printf("| %3s | %20s | %20s | %30s |\n", "ID", "EMAIL", "PASSWORD", "REGISTRATION");
         for (User u : InMemoryDB.tableUser) {
-            System.out.printf("| %3d | %20s | %20s | %20s |\n",
+            System.out.printf("| %3d | %20s | %20s | %30s |\n",
                     u.getUserId(), u.getEmail(), u.getPassword(), u.getRegistrationDateTime());
         }
     }
